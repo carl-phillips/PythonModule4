9baebca87c80a8d82236fc4a3433dc8ab3ac34cf
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:35:38 -0500

    TRY - Failing second test

--- venv/test/test_validation_with_try.py
+++ venv/test/test_validation_with_try.py
@@ -7,5 +7,10 @@ class testCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             validation_with_try.average(-90, 89, 78)
 
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90, -89, 78)
+
+
 if __name__ == '__main__':
     unittest.main()
