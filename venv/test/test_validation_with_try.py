c53878a42971f67afd1228b23459d9c4df2ba069
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:34:28 -0500

    TRY - fixed failing first test

--- venv/test/test_validation_with_try.py
+++ venv/test/test_validation_with_try.py
@@ -5,7 +5,7 @@ from input_validation import validation_with_try
 class testCase(unittest.TestCase):
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
-            average(-90, 89, 78)
+            validation_with_try.average(-90, 89, 78)
 
 if __name__ == '__main__':
     unittest.main()
