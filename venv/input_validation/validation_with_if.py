c947b8e32c31aadbf45391af350974a647b150cf
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:17:19 -0500

    Initial commit, test failing

---
+++ venv/input_validation/validation_with_if.py
@@ -0,0 +1,17 @@
+
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+    avg = (int(score1) + int(score2) + int(score3)) / NUMBER_TESTS
+    print(str(avg));
+    return avg
+
+
+if __name__ == "__main__":
+    fname = input("Your first name: ")
+    lname = input("Your last name: ")
+    age = input("Your age: ")
+    score1 = input("First score: ")
+    score2 = input("Second score: ")
+    score3 = input("Third score: ")
+    average(score1, score2, score3)
+
---
+++ venv/test/test_validation_with_if.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import mock
+from input_validation import validation_with_if
+
+class testCase(unittest.TestCase):
+    def test_input_valsidation_if(self):
+        self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
+
+if __name__ == '__main__':
+    unittest.main()
