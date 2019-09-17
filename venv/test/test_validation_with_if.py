219985c9ac483b03fbe3b350a58161707868b599
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:23:54 -0500

    Second failing test

--- venv/test/test_validation_with_if.py
+++ venv/test/test_validation_with_if.py
@@ -6,5 +6,8 @@ class testCase(unittest.TestCase):
     def test_input_validation_if(self):
         self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
 
+    def test_second_negative(self):
+        self.assertEqual(-1, validation_with_if.average(90, -92, 98))
+
 if __name__ == '__main__':
     unittest.main()
