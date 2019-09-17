754384225a40e60f7b0e606c0a78954957774e57
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:20:33 -0500

    Initial commit, test failing

--- venv/test/test_validation_with_if.py
+++ venv/test/test_validation_with_if.py
@@ -3,7 +3,7 @@ from unittest import mock
 from input_validation import validation_with_if
 
 class testCase(unittest.TestCase):
-    def test_input_valsidation_if(self):
+    def test_input_validation_if(self):
         self.assertEqual(-1, validation_with_if.average(-90, 90, 95))
 
 if __name__ == '__main__':
