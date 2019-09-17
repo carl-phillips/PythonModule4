1df3ff2127f2f6637ccf2902881a8c067057213a
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-16 19:37:43 -0500

    TRY - Failing third test

--- venv/test/test_validation_with_try.py
+++ venv/test/test_validation_with_try.py
@@ -3,14 +3,17 @@ from unittest import mock
 from input_validation import validation_with_try
 
 class testCase(unittest.TestCase):
-    def test_average_negative_input(self):
+    def test_average_negative_input_1(self):
         with self.assertRaises(ValueError):
             validation_with_try.average(-90, 89, 78)
 
-    def test_average_negative_input(self):
+    def test_average_negative_input_2(self):
         with self.assertRaises(ValueError):
             validation_with_try.average(90, -89, 78)
 
+    def test_average_negative_input_3(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90, 98, -78)
 
 if __name__ == '__main__':
     unittest.main()
